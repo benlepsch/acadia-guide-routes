9cc7866246d96ed94e18032af256d475ebaad170
Ben Lepsch <blepsch@gmail.com>
2024-06-10 13:43:42 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'agr';
+  title:string = 'Acadia Guide Routes';
+
+  selected_area:string = 'otter_cliffs';
+  
+  // import from json file, this is just to test
+  // metadata:any = {
+  //   'otter_cliffs': {
+  //     'drive_time': '15 minutes',
+  //     'approach_time' : '5 minutes'
+  //   }
+  // }
+
+  // so we could just request this from my github site
+  // sans pictures
+  // should i do that first? or go straight to mongo
+  routes:any = {
+    'otter_cliffs': [
+      {
+        'name': 'razorback',
+        'grade': '5.5',
+        'site': 'top',
+        'fortym': 'yes',
+        'pic': 'url?',
+        'anchor': 'url?',
+        'beta': 'lower to ledge above the bottom, easy climbing to a big step out right'
+      }
+    ]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
