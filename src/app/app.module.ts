de8ea1b7163d424fe7a0dc8e419ee753d5d5ca84
Ben Lepsch <blepsch@gmail.com>
2024-06-10 13:47:13 -0400

    update readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
