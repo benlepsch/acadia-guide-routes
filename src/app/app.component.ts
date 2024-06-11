66b703aa77b754c6829d7fca02b4df6663eb336d
Ben Lepsch <blepsch@gmail.com>
2024-06-11 11:46:56 -0400

    clean up a little bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,35 +30,8 @@ export class AppComponent {
 
   receiveData(d: any) : void {
     this.routes = d;
-    console.log(d);
+    // console.log(d);
   }
 
-
-
   selected_area:string = 'otter_cliffs';
-  
-  // import from json file, this is just to test
-  // metadata:any = {
-  //   'otter_cliffs': {
-  //     'drive_time': '15 minutes',
-  //     'approach_time' : '5 minutes'
-  //   }
-  // }
-
-  // so we could just request this from my github site
-  // sans pictures
-  // should i do that first? or go straight to mongo
-  // routes:any = {
-  //   'otter_cliffs': [
-  //     {
-  //       'name': 'razorback',
-  //       'grade': '5.5',
-  //       'site': 'top',
-  //       'fortym': 'yes',
-  //       'pic': 'url?',
-  //       'anchor': 'url?',
-  //       'beta': 'lower to ledge above the bottom, easy climbing to a big step out right'
-  //     }
-  //   ]
-  // }
 }
